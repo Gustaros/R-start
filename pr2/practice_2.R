9a16c5e85172206178642ed97ed7d27b51b8af47
Gustaros <89577581+Gustaros@users.noreply.github.com>
2024-03-01 12:53:46 +0300

    frfrfr

---
+++ pr2/practice_2.R
@@ -0,0 +1,19 @@
+
+
+# Part 1 Task 1 -----------------------------------------------------------
+
+M = matrix(3,nrow=3, ncol=3)
+M
+
+
+
+# NOT RUN {
+diff(1:10, 2)
+diff(1:10, 2, 2)
+x <- cumsum(cumsum(1:10))
+diff(x, lag = 2)
+diff(x, differences = 1)
+diff(x, differences = 2)
+
+diff(.leap.seconds)
+# }
